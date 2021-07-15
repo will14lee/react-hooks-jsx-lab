328f050caaf148b189f9a12efffb17a8796f6b90
will14lee <will14lee@gmail.com>
2021-07-15 05:47:49 -0400

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Home from "../components/Home"
+import About from "../components/About"
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  const home= "#home"
+  const about= "#about"
+  console.log(home)
+  return (
+  <nav>
+    <a href={home}>Home</a>
+    <a href={about}>About</a>
+  </nav>
+  );
 }
 
 export default NavBar;
